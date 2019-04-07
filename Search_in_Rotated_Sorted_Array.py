02a4169e96ac954cd2d72fa69a5c1bf489aaadfe
yashpanchamia <yashpanchamia9@gmail.com>
2019-04-07 16:26:46 -0400

    updated the description

--- Search_in_Rotated_Sorted_Array.py
+++ Search_in_Rotated_Sorted_Array.py
@@ -1,4 +1,4 @@
-'''An sorted array of integers was rotated an unknown number of times.
+'''A sorted array of integers was rotated an unknown number of times.
 
 Given such an array, find the index of the element in the array in faster than linear time. If the element doesn't exist in the array, return null.
 
